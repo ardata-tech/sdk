6e8c6248cdcf1e715433075c32a9511bfdc07058
Val <val.samonte@gmail.com>
2023-08-09 18:10:23 +0800

    Add basic operation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,81 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('dev only output');
+import { OPERATION_SCOPE } from './constants'
+
+export interface DeltaStorageConfig {
+  apiKey: string
+  scope: number
+}
+
+const commandAllowed = (scope: number, flag: number) => {
+  if (scope === 0) return true
+  return (scope & flag) === flag
+}
+
+const verifyAuthorizedCommand = (
+  scope: number,
+  flag: number,
+  message = 'Operation is not allowed.'
+) => {
+  if (!commandAllowed(scope, flag)) {
+    throw Error(message)
   }
-  return a + b;
-};
+}
+
+class DeltaStorageSDK {
+  public readonly scope: number
+
+  constructor(config: DeltaStorageConfig) {
+    // apiKey has 4 parts
+    const [_apiKeyId, scope, _userId, _hash] = config.apiKey.split('.')
+    this.scope = parseInt(scope)
+  }
+
+  readFile() {
+    verifyAuthorizedCommand(
+      this.scope,
+      OPERATION_SCOPE.READ_FILE,
+      'READ_FILE operation is not allowed.'
+    )
+  }
+
+  uploadFile() {
+    verifyAuthorizedCommand(
+      this.scope,
+      OPERATION_SCOPE.UPLOAD_FILE,
+      'UPLOAD_FILE operation is not allowed.'
+    )
+  }
+
+  deleteFile() {
+    verifyAuthorizedCommand(
+      this.scope,
+      OPERATION_SCOPE.DELETE_FILE,
+      'DELETE_FILE operation is not allowed.'
+    )
+  }
+
+  readDirectory() {
+    verifyAuthorizedCommand(
+      this.scope,
+      OPERATION_SCOPE.READ_DIRECTORY,
+      'READ_DIRECTORY operation is not allowed.'
+    )
+  }
+
+  createDirectory() {
+    verifyAuthorizedCommand(
+      this.scope,
+      OPERATION_SCOPE.CREATE_DIRECTORY,
+      'CREATE_DIRECTORY operation is not allowed.'
+    )
+  }
+
+  deleteDirectory() {
+    verifyAuthorizedCommand(
+      this.scope,
+      OPERATION_SCOPE.DELETE_DIRECTORY,
+      'DELETE_DIRECTORY operation is not allowed.'
+    )
+  }
+}
+
+export default DeltaStorageSDK
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,2 @@
-import { sum } from '../src/index';
-
-describe('sum', () => {
-  it('adds two numbers together', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
-});
+const getSampleApiKey = (scope: number) =>
+  `5f1025c4-45db-4ac2-aef3-c595903da003.${scope}.8158e9a6-53b3-4963-a367-8a15f48ff3ce.81JjXqBRuZT8jg62Lzb7kTnKB4Wgdy8zhVtKdACBbRwM`
