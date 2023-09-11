feef69c52643fc9d0acbe4c9cd22ff70d78339bd
Val <val.samonte@gmail.com>
2023-09-11 11:51:58 +0800

    Directory API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { OPERATION_SCOPE } from './constants'
+const axios = require('axios').default
 
 export interface DeltaStorageConfig {
   apiKey: string
-  scope: number
+  host?: string
 }
 
-const commandAllowed = (scope: number, flag: number) => {
+const isCommandAllowed = (scope: number, flag: number) => {
   if (scope === 0) return true
   return (scope & flag) === flag
 }
@@ -15,66 +16,128 @@ const verifyAuthorizedCommand = (
   flag: number,
   message = 'Operation is not allowed.'
 ) => {
-  if (!commandAllowed(scope, flag)) {
+  if (!isCommandAllowed(scope, flag)) {
     throw Error(message)
   }
 }
 
 class DeltaStorageSDK {
+  public readonly apiKey: string
   public readonly scope: number
+  public readonly host: string
 
   constructor(config: DeltaStorageConfig) {
-    // apiKey has 4 parts
     const [_apiKeyId, scope, _userId, _hash] = config.apiKey.split('.')
+    this.apiKey = config.apiKey
     this.scope = parseInt(scope)
+    this.host = config.host ?? ''
+    this.host = this.host.slice(-1) === '/' ? this.host.slice(0, -1) : this.host
   }
 
-  readFile() {
+  async readFile() {
     verifyAuthorizedCommand(
       this.scope,
       OPERATION_SCOPE.READ_FILE,
-      'READ_FILE operation is not allowed.'
+      'READ_FILE is not allowed.'
     )
   }
 
-  uploadFile() {
+  async uploadFile() {
     verifyAuthorizedCommand(
       this.scope,
       OPERATION_SCOPE.UPLOAD_FILE,
-      'UPLOAD_FILE operation is not allowed.'
+      'UPLOAD_FILE is not allowed.'
     )
   }
 
-  deleteFile() {
+  async deleteFile() {
     verifyAuthorizedCommand(
       this.scope,
       OPERATION_SCOPE.DELETE_FILE,
-      'DELETE_FILE operation is not allowed.'
+      'DELETE_FILE is not allowed.'
     )
   }
 
-  readDirectory() {
+  async readDirectory(id?: string) {
     verifyAuthorizedCommand(
       this.scope,
       OPERATION_SCOPE.READ_DIRECTORY,
-      'READ_DIRECTORY operation is not allowed.'
+      'READ_DIRECTORY is not allowed.'
     )
+
+    return axios.get(`${this.host}/api/directory/${id ?? ''}`, {
+      headers: {
+        Authorization: `Bearer ${this.apiKey}`
+      }
+    })
   }
 
-  createDirectory() {
+  async createDirectory(name: string, parentDirectoryId?: string) {
     verifyAuthorizedCommand(
       this.scope,
       OPERATION_SCOPE.CREATE_DIRECTORY,
-      'CREATE_DIRECTORY operation is not allowed.'
+      'CREATE_DIRECTORY is not allowed.'
+    )
+
+    return axios.post(
+      `${this.host}/api/directory/create`,
+      { name, parentDirectoryId },
+      {
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`
+        }
+      }
     )
   }
 
-  deleteDirectory() {
+  async renameDirectory(id: string, name: string) {
+    verifyAuthorizedCommand(
+      this.scope,
+      OPERATION_SCOPE.CREATE_DIRECTORY | OPERATION_SCOPE.DELETE_DIRECTORY,
+      'UPDATE_DIRECTORY is not allowed.'
+    )
+
+    return axios.put(
+      `${this.host}/api/directory/${id}`,
+      { name },
+      {
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`
+        }
+      }
+    )
+  }
+
+  async move(parentId: string, childrenIds: string[]) {
+    verifyAuthorizedCommand(
+      this.scope,
+      OPERATION_SCOPE.CREATE_DIRECTORY | OPERATION_SCOPE.DELETE_DIRECTORY,
+      'UPDATE_DIRECTORY is not allowed.'
+    )
+
+    return axios.put(
+      `${this.host}/api/directory/${parentId}`,
+      { move: childrenIds },
+      {
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`
+        }
+      }
+    )
+  }
+
+  async deleteDirectory(id: string) {
     verifyAuthorizedCommand(
       this.scope,
       OPERATION_SCOPE.DELETE_DIRECTORY,
-      'DELETE_DIRECTORY operation is not allowed.'
+      'DELETE_DIRECTORY is not allowed.'
     )
+
+    return axios.delete(`${this.host}/api/directory/${id}`, {
+      headers: {
+        Authorization: `Bearer ${this.apiKey}`
+      }
+    })
   }
 }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,2 +1,60 @@
-const getSampleApiKey = (scope: number) =>
-  `5f1025c4-45db-4ac2-aef3-c595903da003.${scope}.8158e9a6-53b3-4963-a367-8a15f48ff3ce.81JjXqBRuZT8jg62Lzb7kTnKB4Wgdy8zhVtKdACBbRwM`
+import DeltaStorageSDK from '../src/index'
+
+const apiKey =
+  '00000000-0000-0000-0000-000000000000.0.f2e395ca-a158-48d5-9bcc-d00b952a7c2f.31dFnqgGjnh1NbguqD7CKQh7DzvaCe9sraytpWn3E9Lu'
+
+let sdk: DeltaStorageSDK
+let directoryId: string
+let parentDirectoryId: string
+
+beforeAll(() => {
+  sdk = new DeltaStorageSDK({
+    apiKey,
+    host: 'http://localhost:1337'
+  })
+})
+
+test('should be able to create directory', async () => {
+  const name = 'Directory created from test'
+  const directory = await sdk.createDirectory(name)
+  const parent = await sdk.createDirectory('Parent Directory')
+
+  expect(directory.data.name).toEqual(name)
+  directoryId = directory.data.id
+  parentDirectoryId = parent.data.id
+})
+
+test('should be able to read root directory', async () => {
+  const directory = await sdk.readDirectory()
+  expect(directory.data.directories).toBeInstanceOf(Array)
+  expect(directory.data.files).toBeInstanceOf(Array)
+})
+
+test('should be able to read a specific directory', async () => {
+  const directory = await sdk.readDirectory(directoryId)
+  expect(directory.data.directories).toBeInstanceOf(Array)
+  expect(directory.data.files).toBeInstanceOf(Array)
+})
+
+test('should be able to rename a specific directory', async () => {
+  const name = 'Directory new name'
+  const directory = await sdk.renameDirectory(directoryId, name)
+  expect(directory.data.name).toEqual(name)
+})
+
+test('should be able to move to a new directory', async () => {
+  await sdk.move(parentDirectoryId, [directoryId])
+  const directory = await sdk.readDirectory(parentDirectoryId)
+  expect(directory.data.directories).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        id: directoryId
+      })
+    ])
+  )
+})
+
+test('should be able to soft delete a directory', async () => {
+  const result = await sdk.deleteDirectory(directoryId)
+  expect(result.data.success).toBeTruthy()
+})
