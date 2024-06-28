4169de8cda330ce9e33084834e826696acf89de2
Franz <franzegos9@gmail.com>
2024-06-28 14:43:23 +0800

    fix: build error

--- src/types.ts
+++ src/types.ts
@@ -128,3 +128,31 @@ export interface RetrievalRequest {
     cid: string
   }
 }
+
+export interface DataResponse {
+  success: boolean
+  code: number
+  message?: string
+}
+
+export type DataResponsePromise<T = {}, E = {}> = Promise<
+  [(DataResponse & T) | null, error: (DataResponse & E) | null]
+>
+
+export interface EdgeResponse {
+  ID: number
+  name: string
+  size: number
+  cid: string
+  bucket_uuid: string
+  status: string
+  piece_cid: string
+  piece_size: number
+  last_message: string
+  miner: string
+  make_deal: boolean
+  collection_name: string
+  created_at: Date
+  updated_at: Date
+  imageLink: string
+}
